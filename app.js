0f686c6474da8df52a2657f48c7cc47821035a41
Hardik Arora <hardikarora826@gmail.com>
2021-05-18 21:13:23 +0530

    wired output box

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var btnTranslate= document.querySelector("#btn-translate");
+var txtInput= document.querySelector("#txt-input");
+var outputDiv= document.querySelector("#output");
+
 
 function clickEventFun(){
     console.log("clicked")
+    outputDiv.innerText= txtInput.value
 }
 
 btnTranslate.addEventListener("click", clickEventFun);
